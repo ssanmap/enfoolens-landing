6a8a5fa0cf57a883ec21c9a097baab8cfe22baff
Sebastian <sebastian.smp21@gmail.com>
2025-04-09 19:03:01 -0400

    wspw

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/56912345678?text=Hola%20ENFOOLENS,%20quisiera%20cotizar%20lentes"
+      href="https://wa.me/56976611236?text=Hola%20ENFOOLENS,%20quisiera%20cotizar%20lentes"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition z-50 "
